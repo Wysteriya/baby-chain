bc1145268cd92066e25c0e10979f7bc039af74e4
hari-vishwanath <mailtohari134@gmail.com>
2022-11-18 22:28:21 +0530

    Added proof of work algorithm

--- blockchain/block/block.go
+++ blockchain/block/block.go
@@ -1,95 +1,110 @@
 package block
 
 import (
-    "encoding/json"
-    "fmt"
-    "errors"
+	"blockchain/blockchain/Proof_of_work"
+	"encoding/json"
+	"errors"
+	"fmt"
 )
 
 type Block struct {
-    header string
-    timestamp Time
-    prevHash Hash
-    hash Hash
-    data Data
+	header    string
+	timestamp Time
+	prevHash  Hash
+	hash      Hash
+	data      Data
+	nonce     int64
 }
 
 type block struct {
-    Header *string `json:"header"`
-    Timestamp *Time `json:"timestamp"`
-    PrevHash *Hash `json:"prev_hash"`
-    Hash *Hash `json:"hash"`
-    Data *Data `json:"data"`
+	Header    *string `json:"header"`
+	Timestamp *Time   `json:"timestamp"`
+	PrevHash  *Hash   `json:"prev_hash"`
+	Hash      *Hash   `json:"hash"`
+	Data      *Data   `json:"data"`
+	Nonce     *int64  `json:"nonce"`
 }
 
 func (B *block) toBlock() Block {
-    return Block{*B.Header, *B.Timestamp, *B.PrevHash, *B.Hash, *B.Data}
+	return Block{*B.Header, *B.Timestamp, *B.PrevHash, *B.Hash, *B.Data, *B.Nonce}
 }
 
 func (b *Block) MarshalJSON() ([]byte, error) {
-    if err := b.Validate(); err != nil {
-        return []byte{}, err
-    }
-    return json.Marshal(block{&b.header, &b.timestamp, &b.prevHash, &b.hash, &b.data})
+	if err := b.Validate(); err != nil {
+		return []byte{}, err
+	}
+	return json.Marshal(block{&b.header, &b.timestamp, &b.prevHash, &b.hash, &b.data, &b.nonce})
 }
 
 func (b *Block) UnmarshalJSON(data []byte) error {
-    B := block{}
-    if err := json.Unmarshal(data, &B); err != nil {
-        return err
-    }
-    *b = B.toBlock()
-    if err := b.Validate(); err != nil {
-        return err
-    }
-    return nil
+	B := block{}
+	if err := json.Unmarshal(data, &B); err != nil {
+		return err
+	}
+	*b = B.toBlock()
+	if err := b.Validate(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (b *Block) Hash() Hash {
-    return HashB([]byte(b.header), []byte{byte(b.timestamp)}, []byte(b.prevHash.Hex()), []byte(b.data.String()))
+	return HashB([]byte(b.header), []byte{byte(b.timestamp)}, []byte(b.prevHash.Hex()), []byte(b.data.String()))
 }
 
 func (b *Block) PrevHash() Hash {
-    return b.prevHash
+	return b.prevHash
 }
 
+func (b *Block) Data() *Data {
+	return &b.data
+}
+
+func (b *Block) Nonce() int64 {
+	return b.nonce
+}
 func (b *Block) Validate() error {
-    if b.Hash() != b.hash {
-        return errors.New("hash mismatch")
-    }
-    if err := b.data.Validate(); err != nil {
-        return err
-    }
-    return nil
+	if b.Hash() != b.hash {
+		return errors.New("hash mismatch")
+	}
+	if err := b.data.Validate(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (b *Block) Print() {
-    fmt.Printf("Header: %s; Timestamp: %s; PrevHash: %s...; Hash: %s...;\nData: %s\n",
-               b.header, b.timestamp.String(), b.prevHash.Hex()[:16], b.hash.Hex()[:16], b.data)
+	fmt.Printf("Header: %s; Timestamp: %s; PrevHash: %s...; Hash: %s...;\nData: %s\n",
+		b.header, b.timestamp.String(), b.prevHash.Hex()[:16], b.hash.Hex()[:16], b.data)
 }
 
 func (b *Block) Save() ([]byte, error) {
-    return json.Marshal(b)
+	return json.Marshal(b)
 }
 
 func New(header string, timestamp Time, prevHash Hash, data Data) Block {
-    b := Block{header, timestamp, prevHash, HashB(), data}
-    b.hash = b.Hash()
-    return b
+	b := Block{header, timestamp, prevHash, HashB(), data, 0}
+	b.hash = b.Hash()
+	return b
 }
 
 func Load(save []byte) (Block, error) {
-    b := Block{}
-    if err := json.Unmarshal(save, &b); err != nil {
-        return Block{}, err
-    }
-    return b, nil
+	b := Block{}
+	if err := json.Unmarshal(save, &b); err != nil {
+		return Block{}, err
+	}
+	return b, nil
 }
 
 func MBlock(header string, prevHash Hash, data Data) Block {
-    return New(header, CurrTime(), prevHash, data)
+	blk := New(header, CurrTime(), prevHash, data)
+	powService := Proof_of_work.NewPowService(&blk)
+	nonce, hash := powService.Run()
+	blk.nonce = nonce
+	blk.hash = HashB(hash)
+	return blk
 }
 
 func Genesis(data Data) Block {
-    return MBlock("Genesis", HashB(), data)
+	return MBlock("Genesis", HashB(), data)
 }
--- blockchain/blockchain/blockchain.go
+++ blockchain/blockchain/blockchain.go
@@ -1,101 +1,101 @@
 package blockchain
 
 import (
-    "fmt"
-    "errors"
-    "encoding/json"
+	"encoding/json"
+	"errors"
+	"fmt"
 )
 
 import "blockchain/block"
 
 type Blockchain struct {
-    chain []block.Block
-    fork string
+	chain []block.Block
+	fork  string
 }
 
 type blockchain struct {
-    Chain []block.Block `json:"chain"`
-    Fork *string `json:"fork"`
+	Chain []block.Block `json:"chain"`
+	Fork  *string       `json:"fork"`
 }
 
 func (BC *blockchain) toBlockchain() Blockchain {
-    return Blockchain{BC.Chain, *BC.Fork}
+	return Blockchain{BC.Chain, *BC.Fork}
 }
 
 func (bc *Blockchain) MarshalJSON() ([]byte, error) {
-    if err := bc.Validate(); err != nil {
-        return []byte{}, err
-    }
-    return json.Marshal(blockchain{bc.chain, &bc.fork})
+	if err := bc.Validate(); err != nil {
+		return []byte{}, err
+	}
+	return json.Marshal(blockchain{bc.chain, &bc.fork})
 }
 
 func (bc *Blockchain) UnmarshalJSON(data []byte) error {
-    BC := blockchain{}
-    if err := json.Unmarshal(data, &BC); err != nil {
-        return err
-    }
-    *bc = BC.toBlockchain()
-    if err := bc.Validate(); err != nil {
-        return err
-    }
-    return nil
+	BC := blockchain{}
+	if err := json.Unmarshal(data, &BC); err != nil {
+		return err
+	}
+	*bc = BC.toBlockchain()
+	if err := bc.Validate(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (bc *Blockchain) MineBlock(header string, data block.Data) error {
-    b := block.MBlock(header, bc.HashOf(bc.Len() - 1), data)
-    if err := bc.ValidateBlock(b); err != nil {
-        return err
-    }
-    bc.chain = append(bc.chain, b)
-    return nil
+	b := block.MBlock(header, bc.HashOf(bc.Len()-1), data)
+	if err := bc.ValidateBlock(b); err != nil {
+		return err
+	}
+	bc.chain = append(bc.chain, b)
+	return nil
 }
 
 func (bc *Blockchain) Len() int {
-    return len(bc.chain)
+	return len(bc.chain)
 }
 
 func (bc *Blockchain) HashOf(i int) block.Hash {
-    return bc.chain[i].Hash()
+	return bc.chain[i].Hash()
 }
 
 func (bc *Blockchain) PrevHashOf(i int) block.Hash {
-    return bc.chain[i].PrevHash()
+	return bc.chain[i].PrevHash()
 }
 
 func (bc *Blockchain) Validate() error {
-    return nil
+	return nil
 }
 
 func (bc *Blockchain) ValidateBlock(b block.Block) error {
-    if err := b.Validate(); err != nil {
-        return err
-    }
-    if bc.HashOf(bc.Len() - 1) == b.PrevHash() {
-        return nil
-    }
-    return errors.New("chain hash mismatch")
+	if err := b.Validate(); err != nil {
+		return err
+	}
+	if bc.HashOf(bc.Len()-1) == b.PrevHash() {
+		return nil
+	}
+	return errors.New("chain hash mismatch")
 }
 
 func (bc *Blockchain) Print() {
-    fmt.Println("---")
-    for _, block := range bc.chain {
-        block.Print()
-        fmt.Println("---")
-    }
+	fmt.Println("---")
+	for _, block := range bc.chain {
+		block.Print()
+		fmt.Println("---")
+	}
 }
 
 func (bc *Blockchain) Save() ([]byte, error) {
-    return json.Marshal(bc)
+	return json.Marshal(bc)
 }
 
 func New(data block.Data) Blockchain {
-    return Blockchain{[]block.Block{block.Genesis(data)}, "AA1"}
+	return Blockchain{[]block.Block{block.Genesis(data)}, "AA1"}
 }
 
 func Load(save []byte) (Blockchain, error) {
-    bc := Blockchain{}
-    if err := json.Unmarshal(save, &bc); err != nil {
-        return Blockchain{}, err
-    }
-    return bc, nil
+	bc := Blockchain{}
+	if err := json.Unmarshal(save, &bc); err != nil {
+		return Blockchain{}, err
+	}
+	return bc, nil
 }
