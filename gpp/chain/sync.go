c87a5a48db8abbd71f2e164a60a4e066d0863f68
Hari Vishwanath <mailtohari134@gmail.com>
2022-11-26 02:06:16 +0530

    Sync api

--- gpp/chain/sync.go
+++ gpp/chain/sync.go
@@ -4,12 +4,40 @@ import (
 	"blockchain/block"
 	"blockchain/blockchain"
 	"blockchain/states"
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
 )
 
-func Sync(bc *blockchain.Blockchain, sd *states.StateData) {
+func Sync(bc *blockchain.Blockchain, sd *states.StateData) error {
 	nodes, _ := (*sd)["full_nodes"].(block.Data)
-	for publicKey, ip := range nodes {
-		fmt.Println(publicKey, ip)
+	for _, ip := range nodes {
+		ip, _ := ip.(string)
+		save, err := bc.Save()
+		if err != nil {
+			return err
+		}
+		if err := SendBC(ip, save); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func SendBC(ip string, data []byte) error {
+	dataString := string(data)
+	postBody, _ := json.Marshal(map[string]string{
+		"blockchain_data": dataString,
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	url := ip + ":9090" + "/baby_chain/service/sync"
+	resp, err := http.Post(url, "application/json", responseBody)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("http status error : " + string(rune(resp.StatusCode)))
+	}
+	return nil
 }
--- gpp/services/models.go
+++ gpp/services/models.go
@@ -1,9 +1,22 @@
 package services
 
 type NewNodePost struct {
+	UserType  string `json:"user_type"`
 	IpAddress string `json:"ip_address"`
 }
 type NewNodeResponse struct {
 	PublicKey  string `json:"public_key"`
 	PrivateKey string `json:"private_key"`
+}
+
+type AnnounceTravelPost struct {
+	FromLat string `json:"from_lat"`
+	FromLon string `json:"from_lon"`
+	ToLat   string `json:"to_lat"`
+	ToLon   string `json:"to_lon"`
+	Time    string `json:"time"`
+}
+
+type SyncPost struct {
+	BlockchainData string `json:"blockchain_data"`
 }
