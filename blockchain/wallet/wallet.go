749ad34fe057f9f664e116a13f4ab98c4ab9c4dd
Hari Vishwanath <mailtohari134@gmail.com>
2023-01-03 11:24:05 +0530

    Corrected key pair generation code and wrote tests for wallet

--- blockchain/wallet/wallet.go
+++ blockchain/wallet/wallet.go
@@ -1,98 +1,51 @@
 package wallet
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
+	"crypto/ed25519"
 	"encoding/hex"
 	"fmt"
-	"math/big"
 )
 
-func VerifySignature(publicKey_ string, hash []byte, sig []byte) bool {
-	publicKey := makePublicKey(publicKey_)
-	isValid := ecdsa.VerifyASN1(publicKey, hash, sig)
-	return isValid
-}
-
-func SignMessage(privateKey_ string, publicKey string, hash []byte) ([]byte, error) {
-	privateKey := makePrivateKey(publicKey, privateKey_)
-	sign, err := ecdsa.SignASN1(rand.Reader, privateKey, hash)
-	return sign, err
-}
-
 func GeneratePublicAddressAndKey() (string, string, error) {
-	keys, err := generateKeys()
+	publicKey, privateKey, err := generateKeys()
 	if err != nil {
 		return "", "", err
 	}
-	addrX := keys.PublicKey.X.Bytes()
-	addrY := keys.PublicKey.Y.Bytes()
-	publicAddr := append(addrX, addrY...)
-	privyD := keys.D.Bytes()
-	return hex.EncodeToString(publicAddr), hex.EncodeToString(privyD), nil
+	return hex.EncodeToString(publicKey), hex.EncodeToString(privateKey), nil
 }
 
-func generateKeys() (ecdsa.PrivateKey, error) {
-	publicKeyCurve := elliptic.P256()
-	privateKey, err := ecdsa.GenerateKey(publicKeyCurve, rand.Reader)
-	return *privateKey, err
+func SignMessage(privateKey_ string, hash []byte) []byte {
+	privateKey := makePrivateKey(privateKey_)
+	sign := ed25519.Sign(privateKey, hash)
+	return sign
 }
 
-func makePrivateKey(publicAddress string, privyD string) *ecdsa.PrivateKey {
-	pubX, err1 := hex.DecodeString(publicAddress[:64])
+func VerifySignature(publicKey_ string, hash []byte, sig []byte) bool {
+	publicKey := makePublicKey(publicKey_)
+	isValid := ed25519.Verify(publicKey, hash, sig)
+	return isValid
+}
+
+func generateKeys() (publicKey ed25519.PublicKey, privateKey ed25519.PrivateKey, err error) {
+	publicKey, privateKey, err = ed25519.GenerateKey(nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	return publicKey, privateKey, nil
+}
+
+func makePrivateKey(_privateKey string) ed25519.PrivateKey {
+	privateKey, err1 := hex.DecodeString(_privateKey)
 	if err1 != nil {
 		fmt.Println(err1.Error())
 	}
-	pubY, err2 := hex.DecodeString(publicAddress[64:])
-	if err2 != nil {
-		fmt.Println(err2.Error())
-	}
-	privateD, err3 := hex.DecodeString(privyD[:])
-	if err3 != nil {
-		fmt.Println(err3.Error())
-	}
-	pubKeyX := new(big.Int)
-	pubKeyY := new(big.Int)
-	privateKeyD := new(big.Int)
-
-	pubKeyX.SetBytes(pubX)
-	pubKeyY.SetBytes(pubY)
-	privateKeyD.SetBytes(privateD)
-
-	pubKey := new(ecdsa.PublicKey)
-	keyCurve := elliptic.P256()
-	pubKey.Curve = keyCurve
-	pubKey.X = pubKeyX
-	pubKey.Y = pubKeyY
-
-	privyKey := new(ecdsa.PrivateKey)
-	privyKey.PublicKey = *pubKey
-	privyKey.D = privateKeyD
-
-	return privyKey
+	return privateKey
 }
 
-func makePublicKey(publicAddress string) *ecdsa.PublicKey {
-	pubX, err1 := hex.DecodeString(publicAddress[:64])
+func makePublicKey(publicAddress string) ed25519.PublicKey {
+	publicKey, err1 := hex.DecodeString(publicAddress)
 	if err1 != nil {
 		fmt.Println(err1.Error())
 	}
-	pubY, err2 := hex.DecodeString(publicAddress[64:])
-	if err2 != nil {
-		fmt.Println(err2.Error())
-	}
-	pubKeyX := new(big.Int)
-	pubKeyY := new(big.Int)
-
-	pubKeyX.SetBytes(pubX)
-	pubKeyY.SetBytes(pubY)
-
-	pubKey := new(ecdsa.PublicKey)
-	keyCurve := elliptic.P256()
-	pubKey.Curve = keyCurve
-	pubKey.X = pubKeyX
-	pubKey.Y = pubKeyY
-
-	return pubKey
+	return publicKey
 }
--- blockchain/wallet/wallet_test.go
+++ blockchain/wallet/wallet_test.go
@@ -2,21 +2,22 @@ package wallet
 
 import (
 	"blockchain/block"
+	"fmt"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	publicKey, privateKey, _ := GeneratePublicAddressAndKey()
-	if len(publicKey) != 128 {
+	fmt.Println(publicKey)
+	fmt.Println(privateKey)
+	if len(publicKey) != 64 {
 		t.Fatalf("unstable publicKey")
-	} else if len(privateKey) != 64 {
+	} else if len(privateKey) != 128 {
 		t.Fatalf("unstable privateKey")
 	}
 	hash := block.HashB([]byte("Test"))
-	signature, err := SignMessage(privateKey, publicKey, hash[:])
-	if err != nil {
-		t.Fatal(err)
-	} else if !VerifySignature(publicKey, hash[:], signature) {
+	signature := SignMessage(privateKey, hash[:])
+	if !VerifySignature(publicKey, hash[:], signature) {
 		t.Fatalf("valid signature is not being validated")
 	}
 }
